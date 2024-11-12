711cef019d1f1ba3301cd975dd9ae1cec3a43b7f
Paul Nelissen <paul.nelissen@capgemini.com>
2024-11-12 17:09:49 +0100

    add name and optionally include properties

--- python/src/syntax_tree/ast_shower.py
+++ python/src/syntax_tree/ast_shower.py
@@ -1,29 +1,28 @@
 
 from io import StringIO
 import io
-from typing import IO
 from .ast_node import ASTNode
 
 class ASTShower:
     @staticmethod
-    def show_node(ast_node: ASTNode):
-        print('\n'+ASTShower.get_node(ast_node))
+    def show_node(ast_node: ASTNode, include_properties = False):
+        print('\n'+ASTShower.get_node(ast_node, include_properties))
 
     @staticmethod
-    def get_node(ast_node: ASTNode):
+    def get_node(ast_node: ASTNode, include_properties = False):
         buffer = io.StringIO()
-        ASTShower._process_node(buffer, "", ast_node)
+        ASTShower._process_node(buffer, "", ast_node, include_properties)
         return buffer.getvalue()
 
     @staticmethod
-    def _process_node( output: StringIO, indent, node: 'ASTNode'):
+    def _process_node( output: StringIO, indent, node: 'ASTNode', include_properties):
         if not node.is_part_of_translation_unit():
             return
         
         raw = node.get_raw_signature()
         raw_lines = raw.splitlines()
-
-        output.write(f"{indent}({node.get_kind()}, {node.get_containing_filename()}[{node.get_start_offset()}:{node.get_start_offset()+node.get_length()}]):")
+        properties_text = node.get_properties() if include_properties else ""
+        output.write(f"{indent}({node.get_kind()}, {node.get_name()}, {node.get_containing_filename()}[{node.get_start_offset()}:{node.get_start_offset()+node.get_length()}]){properties_text}:")
         if len(raw_lines) < 2:
             output.write(f" |{raw}|")
         else:
@@ -32,4 +31,4 @@ class ASTShower:
         output.write("\n")
 
         for child in node.get_children():
-            ASTShower._process_node(output, indent + "  ", child)
+            ASTShower._process_node(output, indent + "  ", child, include_properties)
