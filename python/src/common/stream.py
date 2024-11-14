2a4037f0fa6173a3442b3012b3b536913ff607c6
Paul Nelissen <paul.nelissen@capgemini.com>
2024-11-14 14:53:55 +0100

    add Stream tests

--- python/src/common/stream.py
+++ python/src/common/stream.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import TypeVar, Generic, Iterable, Callable, Any, Optional
+from typing import Sequence, TypeVar, Generic, Iterable, Callable, Any, Optional
 from functools import reduce
 
 T = TypeVar('T')
@@ -26,19 +26,21 @@ class StreamOptional(Generic[T]):
 class Stream(Generic[T]):
     """A Stream similar to java.util.Stream"""
     def __init__(self, iterable: Iterable[T]):
-        self.__iterable = iterable
+        self.__iterable = iterable if not isinstance(iterable, Sequence) else iter(iterable)
 
     def to_iterable(self) -> Iterable[T]:
-        return self.__iterable # type: ignore
+        return self.__iterable 
 
     def filter(self, func: Callable[[T], bool]) -> 'Stream[T]':
-        self.__iterable = filter(func, self.__iterable) # type: ignore
+        self.__iterable = filter(func, self.__iterable) 
         return self
 
     def map(self, func_or_type: type[U]|Callable[[T], U|None]) -> 'Stream[U]':
-        if not isinstance(func_or_type, Callable):
-            return Stream(map(Stream.__cast, filter(lambda x: isinstance(x, func_or_type), self.__iterable)))
-        mapped = map(func_or_type, self.__iterable) # type: ignore
+        mapped = None
+        if type(func_or_type) == type:
+            mapped = map(lambda x: Stream.__cast(x,func_or_type), self.__iterable)
+        else: 
+            mapped = map(func_or_type, self.__iterable) 
         filtered = filter(lambda t: t!=None, mapped)
         return Stream(filtered)
 
@@ -47,8 +49,9 @@ class Stream(Generic[T]):
             result = func(x)
             if isinstance(result, Stream):
                 return result.__iterable
+            return result
             
-        flat_map = (item for sublist in map(get_iterable, self.__iterable) for item in sublist) # type: ignore
+        flat_map = (item for sublist in map(get_iterable, self.__iterable) for item in sublist) 
         return Stream(flat_map)
 
     def distinct(self) -> 'Stream[T]':
@@ -57,7 +60,7 @@ class Stream(Generic[T]):
         return self
 
     def sorted(self, key: Optional[Callable[[T], Any]] = None, reverse: bool = False) -> 'Stream[T]':
-        self.__iterable = iter(sorted(self.__iterable, key=key, reverse=reverse)) # type: ignore
+        self.__iterable = iter(sorted(self.__iterable, key=key, reverse=reverse))  # type: ignore
         return self
 
     def peek(self, func: Callable[[T], Any]) -> 'Stream[T]':
@@ -72,71 +75,51 @@ class Stream(Generic[T]):
         self.__iterable = (x for i, x in enumerate(self.__iterable) if i >= n)
         return self
 
-    def action(self, func: Callable[[T], Any]) -> 'Stream[T]':
-        self.__iterable, iter2 = itertools.tee(self.__iterable)
-        for item in iter2:
-            func(item)
-            return self # first item only
-        return self
-
     def for_each(self, func: Callable[[T], Any]) -> None:
         for item in self.__iterable:
-            func(item) # type: ignore
+            func(item) 
 
     def to_list(self) -> list[T]:
-        return list(self.__iterable) # type: ignore
+        return list(self.__iterable) 
 
     def reduce(self, func: Callable[[T, T], T]) -> StreamOptional[T]:
-        initial = next(self.__iterable, None) # type: ignore
-        if initial is None:
-            return StreamOptional(None)
-        return StreamOptional(reduce(func, self.__iterable, func(initial, initial))) 
+        for item in self.__iterable:
+            initial = item
+            return StreamOptional(reduce(func, self.__iterable, initial)) 
+        return StreamOptional(None)
 
     def collect(self, collector: Callable[[Iterable[T]], Any]) -> Any:
-        return collector(self.__iterable) # type: ignore
+        return collector(self.__iterable) 
 
     def count(self) -> int:
         return sum(1 for _ in self.__iterable)
 
     def any_match(self, predicate: Callable[[T], bool]) -> bool:
-        return any(predicate(x) for x in self.__iterable) # type: ignore
+        return any(predicate(x) for x in self.__iterable) 
 
     def all_match(self, predicate: Callable[[T], bool]) -> bool:
-        return all(predicate(x) for x in self.__iterable) # type: ignore
+        return all(predicate(x) for x in self.__iterable) 
 
     def none_match(self, predicate: Callable[[T], bool]) -> bool:
-        return not any(predicate(x) for x in self.__iterable) # type: ignore
+        return not any(predicate(x) for x in self.__iterable) 
     
     def find_first(self) -> StreamOptional[T]:
-        try:
-            return StreamOptional(next(self.__iterable, None)) # type: ignore
-        except StopIteration:
-            return StreamOptional(None)
+        for item in self.__iterable:
+            return StreamOptional(item)
+        return StreamOptional(None)
 
     def find_last(self) -> StreamOptional[T]:
         try:
             # get the latest element from the iterable
-            return StreamOptional(list(self.__iterable)[-1]) # type: ignore
-        except StopIteration:
+            return StreamOptional(list(self.__iterable)[-1]) 
+        except:
             return StreamOptional(None)
 
     def find_any(self) -> StreamOptional[T]:
         return self.find_first()
 
     @staticmethod
-    def __cast(node): 
-        assert isinstance(node, node) 
-        return node
-
-if __name__ == '__main__':
-    # Example usage
-    l = [1, 2, 3, 4, 5, 6, 7, 8]
-
-    # Use the Stream class to chain transformations
-    def multiply_by_10(x):  return x * 10
-    result = Stream(l).filter(lambda x: x % 2 == 0).map(multiply_by_10).find_first().get()
-    print(result)  # Output: [20, 40, 60, 80]
-
-    # Additional operations
-    sum_result = Stream(l).filter(lambda x: x % 2 == 0).map(lambda x: x * 10).reduce(lambda x, y: x + y)
-    print(sum_result)  # Output: 200
+    def __cast(obj, type): 
+        if isinstance(obj, type):
+            return obj
+        return None
