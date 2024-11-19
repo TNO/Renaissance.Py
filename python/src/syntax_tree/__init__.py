57cf31496d938a688918e932f7cfebc712ff357f
Paul Nelissen <paul.nelissen@capgemini.com>
2024-11-19 16:37:05 +0100

    introduces TextUtils

--- python/src/syntax_tree/__init__.py
+++ python/src/syntax_tree/__init__.py
@@ -7,5 +7,9 @@ from .match_finder import (MatchFinder, PatternMatch)
 from .ast_rewriter import (ASTRewriter)
 from .c_pattern_factory import (CPatternFactory)
 from .ast_utils import (ASTUtils)
+from .text_utils import (TextUtils)
 
-__all__ = ['ASTNode','ASTReference', 'VisitorResult' ,'ASTFinder', 'ASTShower', 'ASTFactory', 'MatchFinder', 'PatternMatch', 'ASTRewriter', 'CPatternFactory', 'ASTUtils']
+__all__ = ['ASTNode','ASTReference', 'VisitorResult' ,'ASTFinder', 
+           'ASTShower', 'ASTFactory', 'MatchFinder', 'PatternMatch',
+            'ASTRewriter', 'CPatternFactory', 'ASTUtils'
+            , 'TextUtils']
