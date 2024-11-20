815dbcae9e4e51ee6119acc67157d692d4560bdd
Paul Nelissen <paul.nelissen@capgemini.com>
2024-11-20 09:35:46 +0100

    publish ASTRefactor

--- python/src/syntax_tree/__init__.py
+++ python/src/syntax_tree/__init__.py
@@ -5,11 +5,23 @@ from .ast_shower import (ASTShower)
 from .ast_factory import (ASTFactory)
 from .match_finder import (MatchFinder, PatternMatch)
 from .ast_rewriter import (ASTRewriter)
+from .ast_refactor import (ASTRefactor)
 from .c_pattern_factory import (CPatternFactory)
 from .ast_utils import (ASTUtils)
 from .text_utils import (TextUtils)
 
-__all__ = ['ASTNode','ASTReference', 'VisitorResult' ,'ASTFinder', 
-           'ASTShower', 'ASTFactory', 'MatchFinder', 'PatternMatch',
-            'ASTRewriter', 'CPatternFactory', 'ASTUtils'
-            , 'TextUtils']
+__all__ = [
+    'ASTNode',
+    'ASTReference',
+    'VisitorResult',
+    'ASTFinder',
+    'ASTShower',
+    'ASTFactory',
+    'MatchFinder',
+    'PatternMatch',
+    'ASTRewriter',
+    'CPatternFactory',
+    'ASTUtils',
+    'TextUtils',
+    'ASTRefactor'
+]
