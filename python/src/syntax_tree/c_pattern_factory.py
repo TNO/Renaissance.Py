7ed17a0647ed2f1e69f6040e433d60da158c0dfc
Paul Nelissen <paul.nelissen@capgemini.com>
2024-12-02 11:58:59 +0100

    Improve useability after test

--- python/src/syntax_tree/c_pattern_factory.py
+++ python/src/syntax_tree/c_pattern_factory.py
@@ -2,6 +2,7 @@ import re
 from typing import Generic, Optional, Sequence
 
 from common.stream import Stream
+from .cpp_utils import CPPUtils
 from .ast_node import ASTNode, ASTNodeType
 from .ast_shower import ASTShower
 
@@ -47,20 +48,22 @@ class CPatternFactory(Generic[ASTNodeType]):
         fullText = self.header + '\n'.join(CPatternFactory._to_declaration(keywords)) + f'\nint {CPatternFactory.reserved_name} = ({text});'
         root =  self._create( fullText)
         #return the first expression found in the tree as a ASTNode
-        return  ASTFinder.find_kind(root, '(?i)PAREN_?EXPR').find_last().get().get_children()[0]
+        return  ASTFinder.find_kind(root.get_children()[-1], '(?i)PAREN_?EXPR').\
+            filter(ASTNode.is_part_of_translation_unit).find_last().get().get_children()[0]
 
     def create_declarations(self, text:str, types: Sequence[str] = [] , parameters: Sequence[str] = [], extra_declarations: Sequence[str] = []):
-        return self._create_body(text, types, parameters, extra_declarations)
+        return self._create_body(text, types, parameters, extra_declarations, '(?i).*DECL.*')
 
     def create_declaration(self, text:str, types: Sequence[str] = [] , parameters: Sequence[str] = [], extra_declarations: Sequence[str] = []):
-        declarations = list(self.create_declarations(text, types, parameters))
-        assert len(declarations) == 1, "Only one declaration is expected"
+        declarations = self.create_declarations(text, types, parameters, extra_declarations)
+        assert len(declarations) > 0, "At least one declaration is expected"
         return declarations[0]
+
     
     def create_statements(self, text:str, types: Sequence[str] = [], extra_declarations: Sequence[str] = []):
         # create a reference for all used variables excluding the specified types
         parameters = [ par for par in CPatternFactory._get_keywords_from_text(text) if not par in types and not any(par in ed for ed in extra_declarations)]
-        return self._create_body(text, types, parameters, extra_declarations)
+        return self._create_body(text, types, parameters, extra_declarations, '.*')
 
     def create(self, text:str):
         """
@@ -83,7 +86,7 @@ class CPatternFactory(Generic[ASTNodeType]):
         assert len(statements) == 1, "Only one statement is expected"
         return statements[0]
     
-    def _create_body(self, text, types, parameters, extra_declarations):
+    def _create_body(self, text, types, parameters, extra_declarations,kind:str):
         fullText = \
             self.header+\
             '\n'.join(CPatternFactory._to_typedef(types)) +'\n'\
@@ -91,8 +94,14 @@ class CPatternFactory(Generic[ASTNodeType]):
             '\n'.join(extra_declarations) +'\n'\
              '\nvoid '+CPatternFactory.reserved_name+'(){\n' +text +'\n}'
         root =  self._create(fullText)
-        #return the first expression found in the tree as a ASTNode
-        return  ASTFinder.find_kind(root, '(?i)COMPOUND_?STMT').find_first().get().get_children()
+            
+        # from the children of the compound statement that contains the text, get for each child the first 
+        # node of the specified kind
+
+        return Stream(ASTFinder.find_kind(root.get_children()[-1], '(?i)COMPOUND_?STMT').find_first().get().get_children()).\
+            filter(ASTNode.is_part_of_translation_unit).\
+            map(lambda n: ASTFinder.find_kind(n,kind).find_first().get()).\
+            to_list()
 
     def _create(self, text:str)-> ASTNodeType:  
         atu =  self.factory.create_from_text( text, 'test.' + self.language)
@@ -103,7 +112,7 @@ class CPatternFactory(Generic[ASTNodeType]):
     def _get_keywords_from_text(text:str) -> Sequence[str]:
         # regex to get keywords that start with one of two dollars followed by a \\w+
         pattern = re.compile(r'\${0,2}[a-zA-Z]\w*')
-        return list(set(re.findall(pattern, text)))
+        return list(k for k in set(re.findall(pattern, text)) if k not in CPPUtils.RESERVED_KEYWORDS )
 
     @staticmethod
     def _get_dollar_keywords_from_text(text:str) -> Sequence[str]:
@@ -129,6 +138,32 @@ class CPPPatternFactory(CPatternFactory):
 
     def __init__(self, factory: ASTFactory, refNode: Optional[ASTNode] = None):
         super().__init__(factory, refNode, 'cpp')
+
+    def create_constructor_chain_initializer(self, pattern ):
+        class_and_args = re.match(R'([$\w]+)\(([^)]+)\)', pattern.replace(' ',''))
+        if class_and_args:
+            class_name = class_and_args.group(1)
+            args = class_and_args.group(2).split(',')
+        return self._create_constructor_chain_initializer(class_name, args)
+
+
+    def _create_constructor_chain_initializer(self, class_name:str, args: Sequence[str] = [] ):
+        arg_call_string = ','.join(args)
+        arg_decl_string = ','.join('int '+ arg for arg in args)
+        code = f"""
+            class {class_name}{{
+            public:
+                {class_name}({arg_decl_string}) {{}}
+            }};
+            class derived : public {class_name}{{
+            public:
+                derived({arg_decl_string}) : {class_name}({arg_call_string}) {{ }}
+           }};
+        """
+        root = self.factory.create_from_text(code, 'test' + self.language)
+        return  ASTFinder.find_kind(root.get_children()[-1], '(?i)Call_?Expr').\
+           find_first().get()
+
 
 if __name__ == "__main__":
     print(CPatternFactory._get_dollar_keywords_from_text('struct $type;struct $name; $type a = $name; int b = 4; $$x = $$y'))
