786c6a7d625d06fbed26016ae60fcff054b7c8cf
Paul Nelissen <paul.nelissen@capgemini.com>
2024-12-02 11:56:52 +0100

    publish ConstrainedPattern and CPPUtils

--- python/src/syntax_tree/__init__.py
+++ python/src/syntax_tree/__init__.py
@@ -4,12 +4,13 @@ from .ast_finder import (ASTFinder)
 from .ast_shower import (ASTShower)
 from .ast_factory import (ASTFactory)
 from .batch_ast_processor import (BatchASTProcessor, IterableProvider, AST_FACTORY_AND_ATU, Action)
-from .match_finder import (MatchFinder, PatternMatch)
+from .match_finder import (MatchFinder, PatternMatch, ConstrainedPattern)
 from .ast_rewriter import (ASTRewriter)
 from .ast_processor import (ASTProcessor)
 from .c_pattern_factory import (CPatternFactory)
 from .ast_utils import (ASTUtils)
 from .text_utils import (TextUtils)
+from .cpp_utils import (CPPUtils)
 
 __all__ = [
     'ASTNode',
@@ -21,8 +22,10 @@ __all__ = [
     'ASTFactory',
     'MatchFinder',
     'PatternMatch',
+    'ConstrainedPattern',
     'ASTRewriter',
     'CPatternFactory',
+    'CPPUtils',
     'ASTUtils',
     'TextUtils',
     'ASTProcessor',
