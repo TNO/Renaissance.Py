4e4a0a32e653a7cef1429606ee903d97d22af624
Pierre van de Laar <pierre.vandelaar@tno.nl>
2025-12-09 09:25:33 +0100

    Added test cases - expressions and statements should match - yet they DO

--- python/test/examples/test_descendant_search.py
+++ python/test/examples/test_descendant_search.py
@@ -1,10 +1,9 @@
 from unittest import TestCase
 from parameterized import parameterized
-from syntax_tree.match_finder import MatchFinder
 
 from examples.descendant_search import find_descendant_match
 from test.c_cpp.factories import Factories
-from syntax_tree import CPatternFactory, ASTFactory, ASTShower
+from syntax_tree import CPatternFactory, ASTFactory, ASTShower, MatchFinder
 
 
 class TestFindDescendantMatch(TestCase):
@@ -31,8 +30,6 @@ class TestFindDescendantMatch(TestCase):
     outer_text: str = "if ($cond) { $$stmts; }"
     inner_text: str = "my_function()"
     extra_declarations_inner_text: list[str] = ["int my_function();"]
-    # inner_text: str = "$f()"
-    # extra_declarations_inner_text: list[str] = ["int $f();"]
 
     @parameterized.expand(Factories.factories)
     def test_descendant_search(self, _: str, factory: ASTFactory):
@@ -42,22 +39,13 @@ class TestFindDescendantMatch(TestCase):
         inner_pattern = pattern_factory.create_expression(
             self.inner_text, self.extra_declarations_inner_text
         )
-        # ASTShower.show_node(code_pattern)
-        # ASTShower.show_node(outer_pattern)
-        # ASTShower.show_node(inner_pattern)
         results = find_descendant_match(
             code_pattern, outer_pattern, inner_pattern
         ).to_list()
 
-        # print("========== found =================")
-        # for result in results:
-        #     ASTShower.show_nodes(result.src_nodes)
-        # print("==================================")
-        # ASTShower.show_nodes(result.get_nodes()["$f"])
-        # ASTShower.show_nodes(result.get_nodes()["$cond"])
-
         count: int = len(results)
         assert 3 == count, "count = " + str(count)
+
 
 class TestBasic(TestCase):
 
@@ -70,26 +58,56 @@ class TestBasic(TestCase):
 
     literal_text: str = "my_function()"
     extra_declarations_literal_text: list[str] = ["int my_function();"]
-    
+
     placeholder_text: str = "$f()"
     extra_declarations_placeholder_text: list[str] = ["int $f();"]
 
-    @parameterized.expand(list(Factories.extend([
-        (literal_text, extra_declarations_literal_text),
-        (placeholder_text, extra_declarations_placeholder_text),
-    ])))
-    def test_snippet(self, _: str, factory: ASTFactory, snippet: str, extra_declarations: list[str]):
-        pattern_factory = CPatternFactory(factory)
-        code_pattern = factory.create_from_text(self.code_text, "text.c")
-        snippet_pattern = pattern_factory.create_expression(
-            snippet, extra_declarations
+    @parameterized.expand(
+        list(
+            Factories.extend(
+                [
+                    (literal_text, extra_declarations_literal_text),
+                    (placeholder_text, extra_declarations_placeholder_text),
+                ]
+            )
         )
+    )
+    def test_snippet(
+        self, _: str, factory: ASTFactory, snippet: str, extra_declarations: list[str]
+    ):
+        pattern_factory = CPatternFactory(factory)
+        code_pattern = factory.create_from_text(
+            self.code_text, "text.c"
+        )  # file extension consistent with C Pattern Factory
+        snippet_pattern = pattern_factory.create_expression(snippet, extra_declarations)
         results = MatchFinder.find_all(code_pattern, [snippet_pattern]).to_list()
-        ASTShower.show_node(code_pattern)
-        ASTShower.show_node(snippet_pattern)
-        print("========== found =================")
-        for result in results:
-            ASTShower.show_nodes(result.src_nodes)
-        print("==================================")
         count: int = len(results)
         assert 1 == count, "count = " + str(count)
+
+
+    @parameterized.expand(Factories.factories)
+    def test_is_match_expression(self, _: str, factory: ASTFactory):
+        pattern_factory = CPatternFactory(factory)
+        expression1_pattern = pattern_factory.create_expression("f()", ["int f();"])
+        assert MatchFinder.is_match(expression1_pattern, expression1_pattern), "An expression matches itself"
+        
+        expression2_pattern = pattern_factory.create_expression("f()", ["int f();"])
+        assert MatchFinder.is_match(expression1_pattern, expression2_pattern), "Identical expressions match"
+        
+        statement_pattern = pattern_factory.create_statement("f();", extra_declarations=["int f();"])
+        assert not MatchFinder.is_match(expression1_pattern, statement_pattern), "An expression doesn't match a statement"
+        
+    @parameterized.expand(Factories.factories)
+    def test_is_match_statement(self, _: str, factory: ASTFactory):
+        pattern_factory = CPatternFactory(factory)
+        statement1_pattern = pattern_factory.create_statement("f();", extra_declarations=["int f();"])
+        assert MatchFinder.is_match(statement1_pattern, statement1_pattern), "A statement matches itself"
+        
+        statement2_pattern = pattern_factory.create_statement("f();", extra_declarations=["int f();"])
+        assert MatchFinder.is_match(statement1_pattern, statement2_pattern), "Identical statements match"
+        
+        expression_pattern = pattern_factory.create_expression("f()", ["int f();"])
+        assert not MatchFinder.is_match(statement1_pattern, expression_pattern), "A statement doesn't match an expression"
+        
+        
+   
