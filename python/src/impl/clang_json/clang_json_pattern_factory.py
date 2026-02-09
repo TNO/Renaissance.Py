1bdb1b913c60092c3531d2dc2e600e91c1b2e043
Jinmin Hu <jinmin.hu@capgemini.com>
2026-02-09 19:12:29 +0100

    fix all unimplemented tests

---
+++ python/src/impl/clang_json/clang_json_pattern_factory.py
@@ -0,0 +1,10 @@
+import unittest
+import ast
+from parameterized import parameterized
+from impl.python.python_pattern_factory import PythonPatternFactory
+
+class ClangPatternFactoryTestCase(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
