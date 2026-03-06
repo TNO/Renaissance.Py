7fdb25c84b1a4d96059173d266d02e1c9a0a38f1
Jinmin Hu <jinmin.hu@capgemini.com>
2026-03-06 15:32:04 +0100

    cover refactoring

--- test/refactoring/test_unit2pytest.py
+++ test/refactoring/test_unit2pytest.py
@@ -1,14 +1,13 @@
 import hamcrest
-from hamcrest import assert_that, is_
+from black import Path
+from hamcrest import assert_that, is_, contains_string
 
 from renaissance.impl.python import PythonASTNode
 from renaissance.syntax_tree import ASTRewriter
-from renaissance.refactoring.unit2pytest import remove_class
-
+from renaissance.refactoring.unit2pytest import remove_class, convert_test_cases
 
 code = '''
-class TestExample(TestCase)
-:
+class TestExample(TestCase):
     def  test_fun(self):  
         self.arrage_1.prepare()
         arrange('other stuff')
@@ -20,14 +19,12 @@ class TestExample(TestCase)
 
 
 def test_remove_class():
-    atu = PythonASTNode.load_from_text(code, 'unknown.py')
+    atu = PythonASTNode.load_from_text(code, Path('unknown.py'),[],None)
     result = remove_class(atu)
-    assert_that(result, is_('')) #not hamcrest.contains_string('class TestExample'))
+    assert_that(result, not contains_string('class TestExample'))
 
-def convert(atu):
-    rewriter = ASTRewriter(atu)
-    pattern_factory = PythonPatternFactory(factory, atu)
-    # remove_class(pattern_factory, atu, rewriter)
-    convert_test_cases(pattern_factory, atu, rewriter)
-    rewriter.apply()
-    return rewriter.apply_to_string()
+def test_convert_test_cases():
+    atu = PythonASTNode.load_from_text(code, Path('unknown.py'),[],None)
+    result = convert_test_cases(atu)
+    assert_that(result, not contains_string('(TestCase)'))
+
