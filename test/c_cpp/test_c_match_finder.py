9920b06bf5985be0780ccf591982e212a14e53fd
Jinmin Hu <jinmin.hu@capgemini.com>
2026-03-12 13:11:11 +0100

    pass 6 manually changet the assert

--- test/c_cpp/test_c_match_finder.py
+++ test/c_cpp/test_c_match_finder.py
@@ -1,5 +1,6 @@
 import logging
-import unittest
+import pytest
+from hamcrest import *
 from unittest import TestCase
 from parameterized import parameterized
 
@@ -12,7 +13,7 @@ from c_cpp.factories import Factories
 
 logger = logging.getLogger(__name__)
 
-class TestCMatchFinder(TestCase):
+class TestCMatchFinder:
 
     SIMPLE_CPP  = """
         void f(){
@@ -38,7 +39,7 @@ class TestCMatchFinder(TestCase):
 
         atu = factory.create_from_text('void fun(){int a,b;\nb--;\na==4;\nb==5;}', "test.c")
         matches = MatchFinder.find_all(atu.children, patterns).to_list()
-        self.assertEqual(1, len(matches))
+        assert_that(matches, has_length(1))
 
 
     def do_test(self, factory: ASTFactory, cpp_code, patterns:list[ASTNode], recursive: bool):
@@ -52,8 +53,8 @@ class TestCMatchFinder(TestCase):
         for actual, expected_dict in zip(actual_matches, expected_dicts_per_match):
             for k, v in actual.expansions.items():
                 for i,n in enumerate(v):
-                    self.assertEqual(expected_dict[k][i], n.text)
-        self.assertEqual(len(expected_dicts_per_match),len(actual_matches))
+                    assert_that(n.text, is_(expected_dict[k][i]))
+        assert_that(actual_matches, has_length(len(expected_dicts_per_match)))
 
 class TestExpressions(TestCMatchFinder):
     def test_match_expr(self):
@@ -66,172 +67,171 @@ class TestExpressions(TestCMatchFinder):
         #find all if and while statements
         matches = MatchFinder.find_all(atu.children,[exprNode]).\
             filter(lambda match: match.nodes[0].is_part_of_translation_unit()).to_list()
-        self.assertEqual(2, len(matches))
+        assert_that(matches, has_length(2))
 
 
-    @parameterized.expand(Factories.extend([
-    ('a == 3',['a==3'], [{}]),
-    ('a == $x',['a==3', 'a==4'], [{'$x':['3']},{'$x':['4']}]),
-    ('$y == $x',['a==3', 'a==4', 'b==5'], [{'$y':['a'], '$x':['3']},{'$y':['a'], '$x':['4']},{'$y':['b'], '$x':['5']}]),
-    ('b--',['b--;'], [{}]),
-    ('b++',[], []),
-    ('--b',[], []),
-    ('++b',[], []),
-    ('$x--',['b--;'], [{'$x': ['b']}]),
-    ('$x++',[], []),
-    ('--$x',[], []),
-    ('++$x',[], []),
-]))
-    def test(self, _, factory, expression, expected_full_matches: list[str], expected_dicts_per_match: list[dict[str, list[str]]]):
-        exprNode = CPatternFactory(factory).create_expression(expression)
-        matches = self.do_test(factory, TestStatements.SIMPLE_CPP, [exprNode], recursive=True)
-        self.assertEqual(expected_full_matches, [compress(match.nodes[0].text) for match in matches])
-        self.assert_matches(expected_dicts_per_match, matches)
+        @pytest.mark.parametrize("_, factory, expression, expected_full_matches, expected_dicts_per_match",Factories.extend([
+        ('a == 3',['a==3'], [{}]),
+        ('a == $x',['a==3', 'a==4'], [{'$x':['3']},{'$x':['4']}]),
+        ('$y == $x',['a==3', 'a==4', 'b==5'], [{'$y':['a'], '$x':['3']},{'$y':['a'], '$x':['4']},{'$y':['b'], '$x':['5']}]),
+        ('b--',['b--;'], [{}]),
+        ('b++',[], []),
+        ('--b',[], []),
+        ('++b',[], []),
+        ('$x--',['b--;'], [{'$x': ['b']}]),
+        ('$x++',[], []),
+        ('--$x',[], []),
+        ('++$x',[], []),
+    ]))
+        def test(self, _, factory, expression, expected_full_matches: list[str], expected_dicts_per_match: list[dict[str, list[str]]]):
+            exprNode = CPatternFactory(factory).create_expression(expression)
+            matches = self.do_test(factory, TestStatements.SIMPLE_CPP, [exprNode], recursive=True)
+            self.assertEqual(expected_full_matches, [compress(match.nodes[0].text) for match in matches])
+            self.assert_matches(expected_dicts_per_match, matches)
 
 class TestStatements(TestCMatchFinder):
         
-    @parameterized.expand(Factories.extend([
-    ('$x;$y;',[{'$x': ['int a = 3;'], '$y': ['int b = 4;']}, {'$x': ['if(a == 3){\n                b=5;\n            }\n            else{\n                b--;\n            }'], '$y': ['while(a != 3){\n                if  (a == 4 && b == 5){\n                    b = a;\n                }\n            }']}]),
-    ('if($x){$$stmts;}',[{'$x': ['a == 4 && b == 5'], '$$stmts': ['b = a;']}]),
-    ('if($x){$$stmts;}else{$single;$$multi;}',[{'$x': ['a == 3'], '$$stmts': ['b=5;'], '$single': ['b--;'], '$$multi': []}]),
-    ('if($x){$$stmts;}else{$$multi;$single;}',[{'$x': ['a == 3'], '$$stmts': ['b=5;'], '$single': ['b--;'], '$$multi': []}]),
-    ('while(a!=$x){$$stmts;}',[{'$x': ['3'], '$$stmts': ['if  (a == 4 && b == 5){\n                    b = a;\n                }']}]),
-]))
-    def test(self, _, factory, statements, expected_dicts_per_match: list[dict[str, list[str]]]):
-        patterns = CPatternFactory(factory).create_statements(statements)
-
-        atu = factory.create_from_text(TestStatements.SIMPLE_CPP, "test.c")
-        func_body = exclude_nodes_by_kind(atu.children)[0].children[2]
-        matches = match_pattern( func_body.children,patterns)
-
-        self.assert_matches( expected_dicts_per_match,matches)
+        @pytest.mark.parametrize("_, factory, statements, expected_dicts_per_match",Factories.extend([
+        ('$x;$y;',[{'$x': ['int a = 3;'], '$y': ['int b = 4;']}, {'$x': ['if(a == 3){\n                b=5;\n            }\n            else{\n                b--;\n            }'], '$y': ['while(a != 3){\n                if  (a == 4 && b == 5){\n                    b = a;\n                }\n            }']}]),
+        ('if($x){$$stmts;}',[{'$x': ['a == 4 && b == 5'], '$$stmts': ['b = a;']}]),
+        ('if($x){$$stmts;}else{$single;$$multi;}',[{'$x': ['a == 3'], '$$stmts': ['b=5;'], '$single': ['b--;'], '$$multi': []}]),
+        ('if($x){$$stmts;}else{$$multi;$single;}',[{'$x': ['a == 3'], '$$stmts': ['b=5;'], '$single': ['b--;'], '$$multi': []}]),
+        ('while(a!=$x){$$stmts;}',[{'$x': ['3'], '$$stmts': ['if  (a == 4 && b == 5){\n                    b = a;\n                }']}]),
+    ]))
+        def test(self, _, factory, statements, expected_dicts_per_match: list[dict[str, list[str]]]):
+            patterns = CPatternFactory(factory).create_statements(statements)
+    
+            atu = factory.create_from_text(TestStatements.SIMPLE_CPP, "test.c")
+            func_body = exclude_nodes_by_kind(atu.children)[0].children[2]
+            matches = match_pattern( func_body.children,patterns)
+    
+            self.assert_matches( expected_dicts_per_match,matches)
 
 class TestFunctionCallStatements(TestCMatchFinder):
 
-    @parameterized.expand(Factories.extend([
-    ('$f($a);',['int $f(int);'],[{'$f': ['one'], '$a': ['a']}]),   
-    ('$f($a, $$all);',['int $f(int,int);'],[{'$f': ['one'], '$a': ['a'], '$$all': []}, {'$f': ['two'], '$a': ['a'], '$$all': ['b']}, {'$f': ['three'], '$a': ['a'], '$$all': ['b', 'c']}]),
-    ('$f($$all, $a);',['int $f(int,int);'],[{'$f': ['one'], '$$all': [], '$a': ['a']}, {'$f': ['two'], '$$all': ['a'], '$a': ['b']}, {'$f': ['three'], '$$all': ['a', 'b'], '$a': ['c']}]),
-    ('$f($a, $$all, $b);',['int $f(int,int,int);'],[{'$f': ['two'], '$a': ['a'], '$$all': [], '$b': ['b']}, {'$f': ['three'], '$a': ['a'], '$$all': ['b'], '$b': ['c']}]),
-]))
-    def test(self, _, factory, statements, extra_declarations, expected_dicts_per_match: list[dict[str, list[str]]]):
-        code = """
-        int one(int a);
-        int two(int a, int b);
-        int three(int a, int b, int c);
-        int a,b,c;
-        void f(){
-            one(a);
-            two(a,b);
-            three(a,b,c);
-        }
-        """
-        
-        stmtNodes = CPatternFactory(factory).create_statements(statements, extra_declarations=extra_declarations)
-        matches = self.do_test(factory, code, stmtNodes, recursive=True)
-        self.assert_matches(expected_dicts_per_match, matches)
+        @pytest.mark.parametrize("_, factory, statements, extra_declarations, expected_dicts_per_match",Factories.extend([
+        ('$f($a);',['int $f(int);'],[{'$f': ['one'], '$a': ['a']}]),   
+        ('$f($a, $$all);',['int $f(int,int);'],[{'$f': ['one'], '$a': ['a'], '$$all': []}, {'$f': ['two'], '$a': ['a'], '$$all': ['b']}, {'$f': ['three'], '$a': ['a'], '$$all': ['b', 'c']}]),
+        ('$f($$all, $a);',['int $f(int,int);'],[{'$f': ['one'], '$$all': [], '$a': ['a']}, {'$f': ['two'], '$$all': ['a'], '$a': ['b']}, {'$f': ['three'], '$$all': ['a', 'b'], '$a': ['c']}]),
+        ('$f($a, $$all, $b);',['int $f(int,int,int);'],[{'$f': ['two'], '$a': ['a'], '$$all': [], '$b': ['b']}, {'$f': ['three'], '$a': ['a'], '$$all': ['b'], '$b': ['c']}]),
+    ]))
+        def test(self, _, factory, statements, extra_declarations, expected_dicts_per_match: list[dict[str, list[str]]]):
+            code = """
+            int one(int a);
+            int two(int a, int b);
+            int three(int a, int b, int c);
+            int a,b,c;
+            void f(){
+                one(a);
+                two(a,b);
+                three(a,b,c);
+            }
+            """
+            
+            stmtNodes = CPatternFactory(factory).create_statements(statements, extra_declarations=extra_declarations)
+            matches = self.do_test(factory, code, stmtNodes, recursive=True)
+            self.assert_matches(expected_dicts_per_match, matches)
 
 class TestMultiAssignments(TestCMatchFinder):
 
-    @parameterized.expand(Factories.extend([
-    ('$f($$all1);$f($$all2);',['int $f(int);'],[{'$f': ['fc'], '$$all1': ['1', '2', '3', '4', '5'], '$$all2': ['1', '2', '6', '4', '5']}]),
-    # skip the advanced undeterministic all placeholder
-    # ('$f($$before, $a, $$after);$f($$before, $b, $$after);',['int $f(int,int,int);'],[{'$f': ['fc'], '$$before': ['1', '2'], '$a': ['3'], '$$after': ['4', '5'], '$b': ['6']}]),
-]))
-    def test_args(self, _, factory, statements, extra_declarations, expected_dicts_per_match: list[dict[str, list[str]]]):
-        code = """
-        int fc(int a, int b, int c, int d, int e);
-        int fc_else(int a, int b, int c, int d, int e);
-        void f(){
-            fc(1,2,3,4,5);
-            fc(1,2,6,4,5);
-
-            fc(1,2,3,4,5);
-            fc_else(1,2,6,4,5);
-        }
-        """
-        
-        stmtNodes = CPatternFactory(factory).create_statements(statements, extra_declarations=extra_declarations)
-        matches = self.do_test(factory, code, stmtNodes, recursive=True)
-        self.assert_matches(expected_dicts_per_match, matches)
-
-    @parameterized.expand(Factories.extend([
-    ('if ($c) {$$before; c=3; $$after;} else {$$before; c=6; $$after;}',[],[{'$c': ['1'], '$$before': ['a=1;', 'b=2;'], '$true': ['c=3;'], '$$after': ['d=4;', 'e=5;'], '$false': ['c=6;']}]),
-]))
-
-    def test_statements(self, _, factory, statements, extra_declarations, expected_dicts_per_match: list[dict[str, list[str]]]):
-        code = """
-        
-        void f(){
-            int a,b,c,d,e;
-            if(1){
-               a=1;
-               b=2;
-               c=3;
-               d=4;
-               e=5;
+        @pytest.mark.parametrize("_, factory, statements, extra_declarations, expected_dicts_per_match",Factories.extend([
+        ('$f($$all1);$f($$all2);',['int $f(int);'],[{'$f': ['fc'], '$$all1': ['1', '2', '3', '4', '5'], '$$all2': ['1', '2', '6', '4', '5']}]),
+        # skip the advanced undeterministic all placeholder
+        # ('$f($$before, $a, $$after);$f($$before, $b, $$after);',['int $f(int,int,int);'],[{'$f': ['fc'], '$$before': ['1', '2'], '$a': ['3'], '$$after': ['4', '5'], '$b': ['6']}]),
+    ]))
+        def test_args(self, _, factory, statements, extra_declarations, expected_dicts_per_match: list[dict[str, list[str]]]):
+            code = """
+            int fc(int a, int b, int c, int d, int e);
+            int fc_else(int a, int b, int c, int d, int e);
+            void f(){
+                fc(1,2,3,4,5);
+                fc(1,2,6,4,5);
+    
+                fc(1,2,3,4,5);
+                fc_else(1,2,6,4,5);
             }
-            else {
-               a=1;
-               b=2;
-               c=6; //different
-               d=4;
-               e=5;
+            """
+            
+            stmtNodes = CPatternFactory(factory).create_statements(statements, extra_declarations=extra_declarations)
+            matches = self.do_test(factory, code, stmtNodes, recursive=True)
+            self.assert_matches(expected_dicts_per_match, matches)
+
+        @pytest.mark.parametrize("_, factory, statements, extra_declarations, expected_dicts_per_match",Factories.extend([
+        ('if ($c) {$$before; c=3; $$after;} else {$$before; c=6; $$after;}',[],[{'$c': ['1'], '$$before': ['a=1;', 'b=2;'], '$true': ['c=3;'], '$$after': ['d=4;', 'e=5;'], '$false': ['c=6;']}]),
+    ]))
+    
+        def test_statements(self, _, factory, statements, extra_declarations, expected_dicts_per_match: list[dict[str, list[str]]]):
+            code = """
+            
+            void f(){
+                int a,b,c,d,e;
+                if(1){
+                   a=1;
+                   b=2;
+                   c=3;
+                   d=4;
+                   e=5;
+                }
+                else {
+                   a=1;
+                   b=2;
+                   c=6; //different
+                   d=4;
+                   e=5;
+                }
             }
-        }
-        """
-        patterns = CPatternFactory(factory).create_statements(statements, extra_declarations=extra_declarations)
-        atu = factory.create_from_text(code, "test.c")
-        func_body = exclude_nodes_by_kind(atu.children)[0].children[2]
-        matches = match_pattern( func_body.children,patterns)
+            """
+            patterns = CPatternFactory(factory).create_statements(statements, extra_declarations=extra_declarations)
+            atu = factory.create_from_text(code, "test.c")
+            func_body = exclude_nodes_by_kind(atu.children)[0].children[2]
+            matches = match_pattern( func_body.children,patterns)
+    
+    
+    
+            self.assert_matches(expected_dicts_per_match,matches)
 
 
-
-        self.assert_matches(expected_dicts_per_match,matches)
 
 class TestUseAtuToCreatePattern(TestCMatchFinder):
-    @parameterized.expand(Factories.extend([
-    ('void f() {const char* bar = BAR;}','(?i)Decl_?Stmt', ['const char* bar = BAR;'], {}),
-    ('void f() {const char* foo = FOO;}','(?i)Decl_?Stmt',['const char* foo = FOO;'], {}),
-    ('void f() {const char* same = SAME;}','(?i)Decl_?Stmt',['const char* same = SAME;'], {}),
-    ('void f() {const char* $name = BAR;}','(?i)Decl_?Stmt',['const char* bar = BAR;'], {'$name':['bar']}),
-    ('void f() {const char* $name = FOO;}','(?i)Decl_?Stmt',['const char* foo = FOO;'] , {'$name':['foo']}),
-    ('void f() {const char* $name = SAME;}','(?i)Decl_?Stmt',['const char* same = SAME;'], {'$name':['same']}),
-    ('const char* $$args; void f() { print($$args);}','(?i)Call_?Expr',['print("%s %s %s", foo, bar, same);'], {'$$args': ['"%s %s %s"', 'foo', 'bar', 'same']}),
-    ]))
-    # @unittest.skip("Macro definitions are currently not included in the AST, so the test cases with FOO, BAR, SAME will fail. Need to implement macro handling first.")
-    def test(self, _, factory, statements, pattern_type, expected, names):
-        code = """
-        #define FOO "foo"
-        #define BAR "bar"
-        #define SAME "bar"
-        typedef struct A_Struct{
-            int a;
-            int b;
-        } A;
-        int some_decl = 1; 
-
-        int print(const char*, ...);
-        void f(){
-            A a = {};
-            const char* foo = FOO;
-            const char* bar = BAR;
-            const char* same = SAME;
-            print("%s %s %s", foo, bar, same);
-
-        }
-        """
-        atu = factory.create_from_text(code, 'test.c')
-        patternFactory = CPatternFactory(factory, ref_node=atu) 
-        statementsAtu = patternFactory.create(statements)
-        statements = ASTFinder.find_kind(statementsAtu, pattern_type).find_last().get()  # pick the last statement
-        # ASTShower.show_node(atu, include_properties=True)
-        # ASTShower.show_node(statementsAtu, include_properties=True)
-        func_body = atu.children[-1].children
-        result = MatchFinder.find_all(func_body, [statements], recursive=True)
-        self.assertLessEqual(1, len(result.to_list()))
-        text=(result.filter(lambda match: match.patterns == names).\
-            map(lambda match: match.nodes[0]).\
-            filter(ASTNode.is_part_of_translation_unit).\
-            map(ASTNode.text).to_list())
-        # self.assertEqual(expected, text)
+        @pytest.mark.parametrize("_, factory, statements, pattern_type, expected, names",Factories.extend([
+        ('void f() {const char* bar = BAR;}','(?i)Decl_?Stmt', ['const char* bar = BAR;'], {}),
+        ('void f() {const char* foo = FOO;}','(?i)Decl_?Stmt',['const char* foo = FOO;'], {}),
+        ('void f() {const char* same = SAME;}','(?i)Decl_?Stmt',['const char* same = SAME;'], {}),
+        ('void f() {const char* $name = BAR;}','(?i)Decl_?Stmt',['const char* bar = BAR;'], {'$name':['bar']}),
+        ('void f() {const char* $name = FOO;}','(?i)Decl_?Stmt',['const char* foo = FOO;'] , {'$name':['foo']}),
+        ('void f() {const char* $name = SAME;}','(?i)Decl_?Stmt',['const char* same = SAME;'], {'$name':['same']}),
+        ('const char* $$args; void f() { print($$args);}','(?i)Call_?Expr',['print("%s %s %s", foo, bar, same);'], {'$$args': ['"%s %s %s"', 'foo', 'bar', 'same']}),
+        ]))
+        def test(self, _, factory, statements, pattern_type, expected, names):
+            code = """
+            #define FOO "foo"
+            #define BAR "bar"
+            #define SAME "bar"
+            typedef struct A_Struct{
+                int a;
+                int b;
+            } A;
+            int some_decl = 1; 
+    
+            int print(const char*, ...);
+            void f(){
+                A a = {};
+                const char* foo = FOO;
+                const char* bar = BAR;
+                const char* same = SAME;
+                print("%s %s %s", foo, bar, same);
+    
+            }
+            """
+            atu = factory.create_from_text(code, 'test.c')
+            patternFactory = CPatternFactory(factory, ref_node=atu) 
+            statementsAtu = patternFactory.create(statements)
+            statements = ASTFinder.find_kind(statementsAtu, pattern_type).find_last().get()  # pick the last statement
+            func_body = atu.children[-1].children
+            result = MatchFinder.find_all(func_body, [statements], recursive=True)
+            assert_that(result.to_list(), has_length(greater_than(1)))  # should find multiple matches, at least the one in the pattern and the one in the function body
+            text=(result.filter(lambda match: match.patterns == names).\
+                map(lambda match: match.nodes[0]).\
+                filter(ASTNode.is_part_of_translation_unit).\
+                map(ASTNode.text).to_list())
+            # self.assertEqual(expected, text)
