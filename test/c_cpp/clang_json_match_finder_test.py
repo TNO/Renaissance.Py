0dfba915f171062341d8efff5dd2d9deca733b48
Jinmin Hu <jinmin.hu@capgemini.coom>
2026-03-13 11:27:52 +0100

    pass 8 clang_match_finder_test clean up manually

--- test/c_cpp/clang_json_match_finder_test.py
+++ test/c_cpp/clang_json_match_finder_test.py
@@ -1,12 +1,11 @@
-import unittest
-from unittest import TestCase
+from hamcrest import *
+
 from renaissance.impl.clang import CPatternFactory
 from renaissance.impl.clang_json import ClangJsonASTNode
-from renaissance.syntax_tree import ASTFactory, ASTFinder, MatchFinder, ASTShower
-from renaissance.syntax_tree.match_finder import exclude_nodes_by_kind
+from renaissance.syntax_tree import ASTFactory, ASTFinder, MatchFinder
 
 
-class ClangMatchJsonFinderTest(TestCase):
+class TestClangJsonMatchFinder:
     def testIsMatchUsingMacroFromAtu(self):
         code = """
         #define BAR "bar"
@@ -14,17 +13,14 @@ class ClangMatchJsonFinderTest(TestCase):
             const char* bar = BAR;
         }
         """
-        # must add define becaus e json does not include macro
-        # define BAR "bar"\n
         statements='void f() {const char* bar = BAR;}'
         pattern_type='(?i)Decl_?Stmt'
-        expected = 'const char* bar = BAR;'
         factory = ASTFactory(ClangJsonASTNode, [])
         atu = factory.create_from_text(code, 'test.c')
-        patternFactory = CPatternFactory(factory, ref_node=atu)
-        statementsAtu = patternFactory.create(statements)
-        statements = ASTFinder.find_kind(statementsAtu, pattern_type).find_last().get()
-        ASTShower.show_node(atu)
-        ASTShower.show_node(statements)
+        pattern_factory = CPatternFactory(factory, ref_node=atu)
+        statements_atu = pattern_factory.create(statements)
+        statements = ASTFinder.find_kind(statements_atu, pattern_type).find_last().get()
+
         result = MatchFinder.match_pattern(atu.children, [statements])
-        self.assertEqual(1, len(result))
+
+        assert_that(result, has_length(1))
