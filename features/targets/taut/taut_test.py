453f006a268f9645fac0e96b02c4c0169f3a0329
lli <lunalixxi@hotmail.com>
2026-04-07 09:23:57 +0200

    replace component name

--- features/targets/taut/taut_test.py
+++ features/targets/taut/taut_test.py
@@ -3,38 +3,38 @@
 # 22-Jun-2010 : description                            #
 #------------------------------------------------------#
 import unittest
-import DDXA
-import OOXA
+import NNXA
+import LLXA
 import TAUT
-import VIPRxUNIT
-import EMRWxTL
+import VIPCxUNIT
+import ABCDxTL
 
 class TestImport(TAUT.TestCase):
     def test_import(self):
-        self.import_and_verify_module('EMRWxTL')
+        self.import_and_verify_module('ABCDxTL')
 
-class FakeEMRWxTL(EMRWxTL):
+class FakeABCDxTL(ABCDxTL):
     @TAUT.log_stub
     def create_test_log(self, test_log_id):
-        test_log = DDXA.Object('EMRWxTL:test_log_struct')
+        test_log = NNXA.Object('ABCDxTL:test_log_struct')
         return test_log
 
-class Test_EMRWxTL(VIPRxUNIT.TestCase):
-    def test_EMRWxTL(self):
-        with TAUT.TestDoubles(emrwxtl=FakeEMRWxTL(None)):
+class Test_ABCDxTL(VIPCxUNIT.TestCase):
+    def test_ABCDxTL(self):
+        with TAUT.TestDoubles(abcdxtl=FakeABCDxTL(None)):
             log = TAUT.Logger()
 
-            test_log_id = DDXA.Object('EMTLXT:DD_test_log_id')
-            test_log = DDXA.Object('EMRWxTL:test_log_struct')
-            test_log = emrwxtl.create_test_log(test_log_id)
+            test_log_id = NNXA.Object('EMTLXT:DD_test_log_id')
+            test_log = NNXA.Object('ABCDxTL:test_log_struct')
+            test_log = abcdxtl.create_test_log(test_log_id)
 
-            file_id = DDXA.Object('EMTLXT:DD_test_log_file_id')
-            file_name = DDXA.Object('EMRWxTL:.retrieve_test_log.file_name')
-            fn = 'EMRWxTL:test_log_struct'
-            file_name[0:len(fn)] = 'EMRWxTL:test_log_struct'
-            test_log, version_mismatch = emrwxtl.retrieve_test_log(file_id, test_log_id, file_name)
+            file_id = NNXA.Object('EMTLXT:DD_test_log_file_id')
+            file_name = NNXA.Object('ABCDxTL:.retrieve_test_log.file_name')
+            fn = 'ABCDxTL:test_log_struct'
+            file_name[0:len(fn)] = 'ABCDxTL:test_log_struct'
+            test_log, version_mismatch = abcdxtl.retrieve_test_log(file_id, test_log_id, file_name)
 
-            emrwxtl.store_test_log(file_id, test_log)
+            abcdxtl.store_test_log(file_id, test_log)
 
 
 if __name__ == '__main__':
