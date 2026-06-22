5938c24de03dbaddb94dfcbb938ed23d2bc59d7c
QuinnCarisCapgemini <quinn.caris@capgemini.com>
2026-06-22 13:02:00 +0200

    Fix cli interface

--- src/rejuvenation/cli.py
+++ src/rejuvenation/cli.py
@@ -1,18 +1,18 @@
 import sys
 from pathlib import Path
 
-from renaissance.impl.python import PythonRstNode
+from renaissance.impl.python.rst_node import PythonRstNode
 from renaissance.impl.python.extractor import PythonExtractor
 from renaissance.project.project_scanner import PythonScanner
 from renaissance.refactoring.python_refactoring import PythonRefactoring
 from renaissance.syntax_tree import ASTShower
 
-if __name__ == "__main__":
+def refactor():
     if sys.argv[1] == "refactor":
         print(f'Refactor {Path(".").resolve()}')
         for file in PythonScanner().find_sources():
-            refactor = sys.argv[2]
-            PythonRefactoring.process(refactor, file)
+            refactoring = sys.argv[2]
+            PythonRefactoring.process(refactoring, file)
 
     if sys.argv[1] == "extract":
         print(f'Extracting {Path(".").resolve()}')
@@ -22,8 +22,12 @@ if __name__ == "__main__":
             extractor.process(file)
         extractor.save_graph(filename)
     if sys.argv[1] == "inspect":
-        print(f"inspect {Path(".").resolve()}")
+        print(f"inspect {Path('.').resolve()}")
         file = sys.argv[2]
         ASTShower.focus = f"|{sys.argv[3]}"
         atu = PythonRstNode.load(Path(file))
         ASTShower.show_node(atu)
+
+
+if __name__ == "__main__":
+    refactor()
